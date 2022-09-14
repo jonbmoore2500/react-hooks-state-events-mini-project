56b3180d38f49ac8f886a373c39766cddae5d330
Jonathan <jonathanmoorevla@gmail.com>
2022-09-14 17:13:08 -0500

    Add filtering functionality

--- src/components/App.js
+++ src/components/App.js
@@ -4,27 +4,24 @@ import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
  
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+// console.log("Here's the data you're working with");
+// console.log({ CATEGORIES, TASKS });
 
 function App() {
   const [tasksToDisplay, setTasksToDisplay] = useState(TASKS)
   const [selectedOption, setSelectedOption] = useState('All')
-  // console.log('selectedOption', selectedOption)
   
   function addNewTask(newObj) {
     setTasksToDisplay([...tasksToDisplay, newObj])
   }
   
-  // handle delete function here with setTasksToDisplay
   function deleteTask(taskToDelete) {
     const newList = tasksToDisplay.filter((task) => task.text !== taskToDelete)
     console.log('newList', newList)
     setTasksToDisplay(newList)
   }
 
-  // filter based on selectedOption
-  const filteredTasks = tasksToDisplay
+  const filteredTasks = tasksToDisplay.filter((task) => selectedOption === "All" || task.category === selectedOption)
 
 
   return (
