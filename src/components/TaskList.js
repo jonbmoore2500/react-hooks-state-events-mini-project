5b318dbbc7931b368112d26427c03618a70be93c
Jonathan <jonathanmoorevla@gmail.com>
2022-09-13 22:59:37 -0500

    Create delete functionality in Task

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import Task from "./Task.js"
 
 function TaskList({tasks}) {
+  const [tasksToDisp, setTasksToDisp] = useState(tasks)
   
-  
+  function deleteTask(textToDelete) {
+    const newList = tasksToDisp.filter((task) => task.text !== textToDelete)
+    setTasksToDisp(newList)
+  }
+
   return (
     <div className="tasks">
-      {tasks.map((task) => (
-        <Task key={task.text} text={task.text} category={task.category} />
+      {tasksToDisp.map((task) => (
+        <Task key={task.text} text={task.text} category={task.category} onDeleteTask={deleteTask}/>
       ))}
     </div>
   );
